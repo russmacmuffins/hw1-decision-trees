ee0c127f4e0911ccfdc651b8f8c0dd96c21fa31d
Russell M <russellmacq@gmail.com>
2020-10-03 20:05:39 -0500

    Implemented confusion matrix

--- src/metrics.py
+++ src/metrics.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def confusion_matrix(actual, predictions):
     """
-    Given predictions (an N-length numpy vector) and actual labels (an N-length 
-    numpy vector), compute the confusion matrix. The confusion 
+    Given predictions (an N-length numpy vector) and actual labels (an N-length
+    numpy vector), compute the confusion matrix. The confusion
     matrix for a binary classifier would be a 2x2 matrix as follows:
 
     [
@@ -11,9 +11,9 @@ def confusion_matrix(actual, predictions):
         [false_negatives, true_positives]
     ]
 
-    YOU DO NOT NEED TO IMPLEMENT CONFUSION MATRICES THAT ARE FOR MORE THAN TWO 
+    YOU DO NOT NEED TO IMPLEMENT CONFUSION MATRICES THAT ARE FOR MORE THAN TWO
     CLASSES (binary).
-    
+
     Compute and return the confusion matrix.
 
     Args:
@@ -27,12 +27,25 @@ def confusion_matrix(actual, predictions):
 
     if predictions.shape[0] != actual.shape[0]:
         raise ValueError("predictions and actual must be the same length!")
-    
-    raise NotImplementedError()
+    TN = 0
+    FP = 0
+    FN = 0
+    TP = 0
+    for i in range(0, predictions.shape[0]):
+        if predictions[i] and actual[i]:
+            TP += 1
+        elif not(predictions[i]) and actual[i]:
+            FN += 1
+        elif not(predictions[i]) and not(actual[i]):
+            TN += 1
+        elif predictions[i] and not(actual[i]):
+            FP += 1
+
+    return np.array([[TN, FP], [FN, TP]])
 
 def accuracy(actual, predictions):
     """
-    Given predictions (an N-length numpy vector) and actual labels (an N-length 
+    Given predictions (an N-length numpy vector) and actual labels (an N-length
     numpy vector), compute the accuracy:
 
     Hint: implement and use the confusion_matrix function!
@@ -51,7 +64,7 @@ def accuracy(actual, predictions):
 
 def precision_and_recall(actual, predictions):
     """
-    Given predictions (an N-length numpy vector) and actual labels (an N-length 
+    Given predictions (an N-length numpy vector) and actual labels (an N-length
     numpy vector), compute the precision and recall:
 
     https://en.wikipedia.org/wiki/Precision_and_recall
@@ -73,7 +86,7 @@ def precision_and_recall(actual, predictions):
 
 def f1_measure(actual, predictions):
     """
-    Given predictions (an N-length numpy vector) and actual labels (an N-length 
+    Given predictions (an N-length numpy vector) and actual labels (an N-length
     numpy vector), compute the F1-measure:
 
    https://en.wikipedia.org/wiki/Precision_and_recall#F-measure
@@ -85,11 +98,10 @@ def f1_measure(actual, predictions):
         predictions (np.array): predicted labels of length N
 
     Output:
-        f1_measure (float): F1 measure of dataset (harmonic mean of precision and 
+        f1_measure (float): F1 measure of dataset (harmonic mean of precision and
         recall)
     """
     if predictions.shape[0] != actual.shape[0]:
         raise ValueError("predictions and actual must be the same length!")
 
     raise NotImplementedError()
-
